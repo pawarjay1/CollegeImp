c3db725e2e789e16ca126f88b178bdcbc7e64371
pawarjay1 <81229505+pawarjay1@users.noreply.github.com>
2024-02-21 16:01:20 +0530

    commit

--- 0.Basics/first.java
+++ 0.Basics/first.java
@@ -1,5 +1,48 @@
+import java.util.*; 
+
 public class first {
+
+    // ublic void func(int a,int b){
+    //     for(int i=0; i<5; i++){
+    //         System.out.println(" "+a+b);
+    //     }
+    // }
+
     public static void main(String args[]) {
-        System.out.println("hello pro world!");
+
+        // first f1 = new first(); 
+
+        // Scanner sc = new Scanner(System.in); 
+
+        // System.out.println("Enter a number");
+        // int a = sc.nextInt(); 
+
+        // System.out.println("you entered "+a);
+        
+        // final int a = 10; 
+        // byte b = 10; 
+
+
+        
+        // f1.func(a,b);
+        
+        // sc.close(); 
+
+        // int arr[] = {10,20,340 ,1,4,5,6,22,1}; 
+
+        // int len = arr.length; 
+
+        // Arrays.sort(arr); 
+
+        // int arr2[][] = new int[5][5];
+
+        // System.out.println(" length is "+ len);
+
+        for(int i=0; i<10; i++){
+            if(i==5){
+                continue; 
+            }
+            System.out.println(i);
+        }
     }
 }
