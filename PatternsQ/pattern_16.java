cef31eeb15976608304a443e774115f24f462471
pawarjay1 <81229505+pawarjay1@users.noreply.github.com>
2024-02-27 22:32:50 +0530

    commit

--- PatternsQ/pattern_16.java
+++ PatternsQ/pattern_16.java
@@ -3,12 +3,18 @@ package PatternsQ;
 public class pattern_16 {
     public static void main(String[] args) {
         int n = 5;
-        for (int i = 1; i < n; i++) {
-            char ch = 'A';
-            for (int j=1; j<i; j++) {
-                System.out.print(ch);
+        for (int i = 0; i < n; i++) {
+            char ch = 'A'; 
+            for (int j=0; j<=i; j++) {
+                System.out.print((char)(int)(ch+i));
             }
             System.out.println();
         }
     }
 }
+
+// A
+// BB
+// CCC
+// DDDD
+// EEEEE
