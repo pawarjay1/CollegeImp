c58adc9b8c7974105e4b0739401ca7a4768cc253
pawarjay1 <81229505+pawarjay1@users.noreply.github.com>
2024-02-29 12:55:58 +0530

    commit

--- Striver Sheet/PatternsQ/pattern_20.java
+++ Striver Sheet/PatternsQ/pattern_20.java
@@ -3,7 +3,7 @@ package PatternsQ;
 public class pattern_20 {
     public static void main(String args[]){
         int n=5;
-        int space = 2*n-2;  
+         
         for(int i=0; i<n*2-1; i++){
             int star=i; 
             
@@ -16,11 +16,7 @@ public class pattern_20 {
             for(int k=0; k<2*n-1; k++){
                 System.out.print("-");
             }
-            if(i<n){
-                space += 2; 
-            }else{
-                space = 2; 
-            }
+            
 
             // space 
             for(int j=0; j<=star; j++){
