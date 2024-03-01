856741f20ac772cc738046b274c969a141aec77c
pawarjay1 <81229505+pawarjay1@users.noreply.github.com>
2024-03-01 22:19:38 +0530

    commit

--- 0.Basics/BasicMaths/digit_count.java
+++ 0.Basics/BasicMaths/digit_count.java
@@ -1,13 +1,17 @@
 package BasicMaths;
 
 public class digit_count {
-    public static void main(String args[]){
-        int n=7789;
+
+    static int counter_func(int n){
         int counter=0; 
         while(n>0){
-            n/=10; 
+            n=n/10; 
             counter++; 
         }
-        System.out.println(counter);
+        return counter; 
+    }
+    public static void main(String args[]){
+        int n=7789;
+        System.out.println("counter : "+counter_func(n));
     }
 }
