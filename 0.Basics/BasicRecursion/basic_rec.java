8647fb5042a9f2695dc35258949a2d79c32e3116
pawarjay1 <81229505+pawarjay1@users.noreply.github.com>
2024-03-02 22:04:48 +0530

    commit

--- 0.Basics/BasicRecursion/basic_rec.java
+++ 0.Basics/BasicRecursion/basic_rec.java
@@ -1,18 +1,22 @@
+// print number from 1 to N 
+
 package BasicRecursion;
 
 public class basic_rec {
-    int count = 0; 
-    void f(){
-        if(count == 5){
-            return; 
-        }
-        System.out.println(count);
-        count++; 
-        f(); 
-    }
-    public static void main(String[] args) {
-        basic_rec b1 = new basic_rec(); 
+    // int count = 0;
 
-        b1.f();
+    void f(int i, int n) {
+        if (i > n) {
+            return;
+        }
+        System.out.println(i);
+        // count++;
+        f(i + 1, n);
+    }
+
+    public static void main(String[] args) {
+        basic_rec b1 = new basic_rec();
+        int n = 10;
+        b1.f(1, n);
     }
 }
