adbac2ea9aded316f9366e0bde7827ec5393362e
pawarjay1 <81229505+pawarjay1@users.noreply.github.com>
2024-03-04 18:35:13 +0530

    commit

--- 0.Basics/BasicRecursion/sum_of_n.java
+++ 0.Basics/BasicRecursion/sum_of_n.java
@@ -1,15 +1,16 @@
 package BasicRecursion;
 
 public class sum_of_n {
-    static int sum(int n){
-        if(n==0){
+    static int sum(int n) {
+        if (n == 0) {
             return 0;
         }
-        return n+sum(n-1);
+        return n + sum(n - 1);
     }
+
     public static void main(String[] args) {
-        int n = 5; 
-        // sum(n); 
+        int n = 2;
+        // sum(n);
         System.out.println(sum(n));
     }
 }
