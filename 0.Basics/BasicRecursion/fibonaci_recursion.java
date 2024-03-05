2b767534c44d6d183cf9c72d8a98171b4cc06e32
pawarjay1 <81229505+pawarjay1@users.noreply.github.com>
2024-03-05 23:50:59 +0530

    commit

--- 0.Basics/BasicRecursion/fibonaci_recursion.java
+++ 0.Basics/BasicRecursion/fibonaci_recursion.java
@@ -1,5 +1,17 @@
 package BasicRecursion;
 
 public class fibonaci_recursion {
-    
+    static int f(int n){
+        if(n<=1) return n; 
+
+        // multiple recursion call
+        int last = f(n-1); 
+        int slast = f(n-2); 
+
+        return last+slast; 
+    }
+    public static void main(String args[]){
+        int n = 4; 
+        System.out.println(f(n));
+    }
 }
