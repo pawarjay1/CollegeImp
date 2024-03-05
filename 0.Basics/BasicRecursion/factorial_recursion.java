f4dbadb57ca32e9327fe1d8a594d74d3e63f95b6
pawarjay1 <81229505+pawarjay1@users.noreply.github.com>
2024-03-06 00:02:11 +0530

    commit

---
+++ 0.Basics/BasicRecursion/factorial_recursion.java
@@ -0,0 +1,13 @@
+package BasicRecursion;
+
+public class factorial_recursion {
+    static int f(int n){
+        if(n<=1) return n; 
+
+        return n * f(n-1); 
+    }
+    public static void main(String args[]){
+        int n = 5; 
+        System.out.println(f(n));
+    }
+}
