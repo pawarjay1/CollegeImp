93a3f96bc201703ffddc256823f956d9b66c6e3d
pawarjay1 <81229505+pawarjay1@users.noreply.github.com>
2024-03-20 13:30:09 +0530

    commit

---
+++ Practice/set 1/pra_2.java
@@ -0,0 +1,8 @@
+public class pra_2 {
+    public static void main(String args[]){
+        System.out.println("you Enter arguments: ");
+        for(int i=0; i<5; i++){
+            System.out.println(args[0]);
+        }
+    }
+}
