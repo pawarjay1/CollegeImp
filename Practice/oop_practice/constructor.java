e73b0528d1ac261720c305e6f7fc1669763e3786
pawarjay1 <81229505+pawarjay1@users.noreply.github.com>
2024-03-30 23:22:19 +0530

    commit

--- Practice/oop_practice/constructor.java
+++ Practice/oop_practice/constructor.java
@@ -1,31 +1,27 @@
 package Practice.oop_practice;
 
 class boy{
+    int id; 
     String name; 
-    int age; 
-    boy(){
-        
+
+    boy(int id,String name){
+        this.id = id; 
+        this.name = name; 
     }
+
     boy(boy b1){
-        System.out.println("constructor is initilized!");
+        this.id = b1.id; 
         this.name = b1.name; 
-        this.age = b1.age; 
     }
 
     void show(){
+        System.out.println(id);
         System.out.println(name);
-        System.out.println(age);
     }
 }
 
 public class constructor {
     public static void main(String[] args) {
-        boy b1 = new boy(); 
-        b1.name = "jay"; 
-        b1.age=21; 
-
-        boy b2 = new boy(b1); 
-
-        b2.show(); 
+         boy b = new boy(10,"jay"); 
     }
 }
