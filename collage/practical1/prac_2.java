737731dc804570184c1370fead801bdc194d2431
pawarjay1 <81229505+pawarjay1@users.noreply.github.com>
2024-06-02 19:38:53 +0530

    commit

--- collage/practical1/prac_2.java
+++
@@ -1,7 +0,0 @@
-package collage.practical1;
-
-public class prac_2 {
-    public static void main(String[] args) {
-        System.out.println("welcome to java");
-    }
-}
--- collage/practical1/prac_8.java
+++
@@ -1,7 +0,0 @@
-package collage.practical1;
-
-public class prac_8 {
-    public static void main(String args[]){
-        System.out.println("j");
-    }
-}
